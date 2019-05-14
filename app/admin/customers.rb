305d715d03976b0a400fa47c845880610d0ce194
Raghu Betina <raghu@firstdraft.com>
2019-05-14 16:42:15 +0000

    Whitelist columns in Customer for ActiveAdmin

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Customer do
+
+ permit_params :email, :password, :phonenumber, :firstname, :lastname, :optin_texts, :optin_email, :date_firstconsultation, :date_lastconsultation, :address_street, :address_city, :address_state, :address_zipcode
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
