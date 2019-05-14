e5a3c40051c0379f97c7baa554ec05be962e243a
Raghu Betina <raghu@firstdraft.com>
2019-05-14 16:43:22 +0000

    Only routine user can modify routine

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,4 +1,14 @@
 class RoutinesController < ApplicationController
+  before_action :current_customer_must_be_routine_customer, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_customer_must_be_routine_customer
+    routine = Routine.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_customer == routine.customer
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @routines = Routine.all
 
