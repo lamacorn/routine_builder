bba106700b516bdc9f0dd4f9730cbbed79d70b74
Raghu Betina <raghu@firstdraft.com>
2019-05-14 16:42:36 +0000

    Whitelist columns in CustomerConcern for ActiveAdmin

--- app/admin/customer_concerns.rb
+++ app/admin/customer_concerns.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register CustomerConcern do
+
+ permit_params :customer_id, :concern_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
